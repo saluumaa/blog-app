671edce22b7b5b6ce84b7c355d478fb0767b53a0
saluumaa <salumahassan02@gmail.com>
2023-07-14 10:12:14 +0300

    Create user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+
+def posts_counter
+  posts.order(created_at: :desc).count
+end
+
+end
+
