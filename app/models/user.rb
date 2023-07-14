41f8fd57c80f224661d2f1fa3a9a587d979843c5
saluumaa <salumahassan02@gmail.com>
2023-07-14 15:28:27 +0300

    Update the migration files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  validates :name, presence: true, length: { maximum: 50 }
+  attribute :posts_counter, :integer, default: 0
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   def posts_counter
     posts.order(created_at: :desc).count
   end
