d57e3b94e31b487e7be74d5b778381ebb3855d27
saluumaa <salumahassan02@gmail.com>
2023-07-14 15:26:42 +0300

    Add test to the user

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    subject { User.create(name: 'Salma', photo: 'My photo', bio: 'It is Salma', posts_counter: 0) }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid if the posts_counter is not an integer' do
+      subject.posts_counter = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid if the posts_counter is less than 0' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'returns the count of posts in descending order' do
+      user = User.create(name: 'Saluum', photo: 'https://example.com/photo.jpg', bio: 'Hello, I am Saluum.')
+
+      post1 = Post.create(title: 'First Post', text: 'This is my first post', author: user)
+      post2 = Post.create(title: 'Second Post', text: 'This is my second post', author: user)
+      post3 = Post.create(title: 'Third Post', text: 'This is my third post', author: user)
+
+      expect(user.posts_counter).to eq(3)
+    end
+  end
+end
