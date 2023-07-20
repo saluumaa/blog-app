3eb7d6ff5e96617bb7326022b52e514ddd9d532b
saluumaa <salumahassan02@gmail.com>
2023-07-20 16:51:13 +0300

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 end
