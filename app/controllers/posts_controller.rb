f5132d04324d4bf954a1e9807b9514654f140512
saluumaa <salumahassan02@gmail.com>
2023-07-27 14:24:11 +0300

    Update posts and add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
+    @like = Like.new
+  end
+
+  def new 
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = current_user.posts.build(post_params)
+    @post.author_id = @user.id
+
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:text)
   end
 end
